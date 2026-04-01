4c78810e4b50a63d4826791a8568040f9293dc5b
Ayman El-Khashab <aymanme@msn.com>
2026-04-01 14:14:49 -0700

    Updated to include the new version of the usermods

--- usermod_v2_hexa_clock/usermod_v2_hexa_clock.h
+++ usermod_v2_hexa_clock/usermod_v2_hexa_clock.h
@@ -35,6 +35,7 @@
 #ifndef USERMOD_SN_PHOTORESISTOR_OFFSET_VALUE
 #define USERMOD_SN_PHOTORESISTOR_OFFSET_VALUE 5
 #endif
+
 /*
  * Usermods allow you to add own functionality to WLED more easily
  * See: https://github.com/Aircoookie/WLED/wiki/Add-own-functionality
@@ -56,20 +57,21 @@
 //class name. Use something descriptive and leave the ": public Usermod" part :)
 class HexaClock : public Usermod {
   private:
+    static const char _name[];
 
-  float referenceVoltage = USERMOD_SN_PHOTORESISTOR_REFERENCE_VOLTAGE;
-  float resistorValue = USERMOD_SN_PHOTORESISTOR_RESISTOR_VALUE;
-  float adcPrecision = USERMOD_SN_PHOTORESISTOR_ADC_PRECISION;
-  int8_t offset = USERMOD_SN_PHOTORESISTOR_OFFSET_VALUE;
+    float referenceVoltage = USERMOD_SN_PHOTORESISTOR_REFERENCE_VOLTAGE;
+    float resistorValue = USERMOD_SN_PHOTORESISTOR_RESISTOR_VALUE;
+    float adcPrecision = USERMOD_SN_PHOTORESISTOR_ADC_PRECISION;
+    int8_t offset = USERMOD_SN_PHOTORESISTOR_OFFSET_VALUE;
 
-  unsigned long readingInterval = USERMOD_SN_PHOTORESISTOR_MEASUREMENT_INTERVAL;
-  // set last reading as "40 sec before boot", so first reading is taken after 20 sec
-  unsigned long lastMeasurement = UINT32_MAX - (USERMOD_SN_PHOTORESISTOR_MEASUREMENT_INTERVAL - USERMOD_SN_PHOTORESISTOR_FIRST_MEASUREMENT_AT);
-  // flag to indicate we have finished the first getTemperature call
-  // allows this library to report to the user how long until the first
-  // measurement
-  bool getLuminanceComplete = false;
-  uint16_t lastLuminance = 0;
+    unsigned long readingInterval = USERMOD_SN_PHOTORESISTOR_MEASUREMENT_INTERVAL;
+    // set last reading as "40 sec before boot", so first reading is taken after 20 sec
+    unsigned long lastMeasurement = UINT32_MAX - (USERMOD_SN_PHOTORESISTOR_MEASUREMENT_INTERVAL - USERMOD_SN_PHOTORESISTOR_FIRST_MEASUREMENT_AT);
+    // flag to indicate we have finished the first getTemperature call
+    // allows this library to report to the user how long until the first
+    // measurement
+    bool getLuminanceComplete = false;
+    uint16_t lastLuminance = 0;
 
     //Private class members. You can declare variables and functions only accessible to your usermod here
     unsigned long lastTime = 0;
@@ -96,51 +98,24 @@ class HexaClock : public Usermod {
     int16_t prevPlaylist;
 
     // These config variables have defaults set inside readFromConfig()
-
     int hours = 0, minutes = 0;
 
-    uint16_t getLuminance()
-    {
-      // http://forum.arduino.cc/index.php?topic=37555.0
-      // https://forum.arduino.cc/index.php?topic=185158.0
-      float volts = analogRead(PHOTORESISTOR_PIN) * (referenceVoltage / adcPrecision);
-      float amps = volts / resistorValue;
-      float lux = amps * 1000000 * 2.0;
-      lastLuminance = uint16_t(lux);
-
-      lastMeasurement = millis();
-      getLuminanceComplete = true;
-      return uint16_t(lux);
-    }
+    uint16_t getLuminance();
 
   public:
     //Functions called by WLED
-
+    
     /*
      * setup() is called once at boot. WiFi is not yet connected at this point.
      * You can use it to initialize variables, sensors or similar.
      */
-    void setup() {
-      Serial.println("Hello from hexa_clock usermod!");
-
-      for(int i=0; i<LEDS_NO; i++)
-      {
-        reverseRoundMap[roundMap[i]] = i;
-        reverseVerticalMap[verticalMap[i]] = i;
-      }
-      // set pinmode
-      pinMode(PHOTORESISTOR_PIN, INPUT);
-    }
-
+    void setup();
 
     /*
      * connected() is called every time the WiFi is (re)connected
      * Use it to initialize network interfaces
      */
-    void connected() {
-      Serial.println("Connected to WiFi!");
-    }
-
+    void connected();
 
     /*
      * loop() is called continuously. Here you can check for events, read sensors, etc.
@@ -152,103 +127,26 @@ class HexaClock : public Usermod {
      * 2. Try to avoid using the delay() function. NEVER use delays longer than 10 milliseconds.
      *    Instead, use a timer check as shown here.
      */
-    void loop() {
-      if (millis() - lastTime > 1000) {
-        if(autoBrightnessEnabled && powerOn)
-        {
-          uint16_t lux = getLuminance();
-          
-          if(lux < nightModeThreshold && nightModeEnabled)
-          {
-            if(!nightModeOn)
-            {
-              nightModeOn = true;
-              prevPreset = currentPreset;
-              
-              prevPlaylist = currentPlaylist;
-              currentPlaylist = -1;
-              //apply black background
-              if(!reverseDigits) applyPreset(3);
-            }
-            
-            bri = nightModeBri;
-          }
-          else {
-            if(nightModeOn)
-            {
-              nightModeOn = false;
-              //if there was a playlist playing on play it again
-              if(prevPlaylist != -1)
-              {
-                currentPlaylist = prevPlaylist;
-              }
-              else 
-              {
-                applyPreset(prevPreset);
-              }
-            }
-            bri = constrain(lux * autoBrightnessACoeff + autoBrightnessBCoeff,autoBrightnessMinBri,255);
-
-          }
-          colorUpdated(CALL_MODE_BUTTON);
-          updateInterfaces(CALL_MODE_BUTTON);
-        }
-
-        
-        hours = hour(localTime);
-        minutes = minute(localTime);
-        lastTime = millis();
-      }
-    }
-
-
+    void loop();
 
     /*
      * addToJsonInfo() can be used to add custom entries to the /json/info part of the JSON API.
      * Creating an "u" object allows you to add custom key/value pairs to the Info section of the WLED web UI.
      * Below it is shown how this could be used for e.g. a light sensor
      */
-    
-    void addToJsonInfo(JsonObject& root)
-    {
-      JsonObject user = root["u"];
-      if (user.isNull()) user = root.createNestedObject("u");
-
-      JsonArray lightArr = user.createNestedArray("Light"); //name
-      lightArr.add(lastLuminance); //value
-      lightArr.add(" lux"); //unit
-    }
-    
-
+    void addToJsonInfo(JsonObject& root);
 
     /*
      * addToJsonState() can be used to add custom entries to the /json/state part of the JSON API (state object).
      * Values in the state object may be modified by connected clients
      */
-    void addToJsonState(JsonObject& root)
-    {
-      //root["user0"] = userVar0;
-    }
-
+    void addToJsonState(JsonObject& root);
 
     /*
      * readFromJsonState() can be used to receive data clients send to the /json/state part of the JSON API (state object).
      * Values in the state object may be modified by connected clients
      */
-    void readFromJsonState(JsonObject& root)
-    {
-      //if (root["bri"] == 255) Serial.println(F("Don't burn down your garage!"));
-      if(root.containsKey("on"))
-      {
-        if(root["on"]==true){
-          powerOn = true;
-        }else
-        {
-          powerOn = false;
-        } 
-      }
-    }
-
+    void readFromJsonState(JsonObject& root);
 
     /*
      * addToConfig() can be used to add custom persistent settings to the cfg.json file in the "um" (usermod) object.
@@ -285,22 +183,7 @@ class HexaClock : public Usermod {
      * 
      * I highly recommend checking out the basics of ArduinoJson serialization and deserialization in order to use custom settings!
      */
-    void addToConfig(JsonObject& root)
-    {
-      JsonObject top = root.createNestedObject("HexClockUsermod");
-      top["displayClock"] = displayClock;
-      top["ledmapEnabled"] = ledmapEnabled;
-      top["autoBrightnessEnabled"] = autoBrightnessEnabled;
-      top["autoBrightnessACoeff"] = autoBrightnessACoeff;
-      top["autoBrightnessBCoeff"] = autoBrightnessBCoeff;
-      top["autoBrightnessMinBri"] = autoBrightnessMinBri;
-      top["nightModeEnabled"] = nightModeEnabled;
-      top["nightModeThreshold"] = nightModeThreshold;
-      top["nightModeBri"] = nightModeBri;
-      top["digitWhite"] = digitWhite;
-      top["reverseDigits"] = reverseDigits;
-    }
-
+    void addToConfig(JsonObject& root);
 
     /*
      * readFromConfig() can be used to read back the custom settings you added with addToConfig().
@@ -317,126 +200,21 @@ class HexaClock : public Usermod {
      * 
      * This function is guaranteed to be called on boot, but could also be called every time settings are updated
      */
-    bool readFromConfig(JsonObject& root)
-    {
-      // default settings values could be set here (or below using the 3-argument getJsonValue()) instead of in the class definition or constructor
-      // setting them inside readFromConfig() is slightly more robust, handling the rare but plausible use case of single value being missing after boot (e.g. if the cfg.json was manually edited and a value was removed)
-
-      JsonObject top = root["HexClockUsermod"];
-
-      bool configComplete = !top.isNull();
-
-      configComplete &= getJsonValue(top["displayClock"], displayClock, true);
-      configComplete &= getJsonValue(top["ledmapEnabled"], ledmapEnabled, true);
-      configComplete &= getJsonValue(top["autoBrightnessEnabled"], autoBrightnessEnabled, true);
-      configComplete &= getJsonValue(top["autoBrightnessACoeff"], autoBrightnessACoeff, 2.0);
-      configComplete &= getJsonValue(top["autoBrightnessBCoeff"], autoBrightnessBCoeff, 0.0);
-      configComplete &= getJsonValue(top["autoBrightnessMinBri"], autoBrightnessMinBri, 10);
-      configComplete &= getJsonValue(top["nightModeEnabled"], nightModeEnabled, true);
-      configComplete &= getJsonValue(top["nightModeThreshold"], nightModeThreshold, 10);
-      configComplete &= getJsonValue(top["nightModeBri"], nightModeBri, 1);
-      configComplete &= getJsonValue(top["digitWhite"], digitWhite, true);
-      configComplete &= getJsonValue(top["reverseDigits"], reverseDigits, false);
-
-      return configComplete;
-    }
-
+    bool readFromConfig(JsonObject& root);
 
     /*
      * handleOverlayDraw() is called just before every show() (LED strip update frame) after effects have set the colors.
      * Use this to blank out some LEDs or set them to a different color regardless of the set effect mode.
      * Commonly used for custom clocks (Cronixie, 7 segment)
      */
-    void handleOverlayDraw()
-    {
-      if(!displayClock)return;
-      int orientation = 0;
-      int digit;
-      //for flat-top (1,3,5) orientations there are 12 pixels
-      int pixelsNo = 13 - (orientation%2);
-      //used when reversing digits
-      bool reverseMask[LEDS_NO] = {false};
-      uint8_t local_hours = !useAMPM ? hours : (hours>12 ? hours-12 : (hours==0 ? 12 : hours));
+    void handleOverlayDraw();
 
-      for(int p = 0; p < 4; p++)
-      {
-        switch(p){
-          case 0:
-            digit = local_hours/10;
-            break;
-          case 1:
-            digit = local_hours%10;
-            break;
-          case 2:
-            digit = minutes/10;
-            break;
-          case 3:
-            digit = minutes%10;
-            break;                                  
-        }
-
-        // iterate through every pixel of the digit mask
-        for(int i=0; i<pixelsNo; i++){
-          //check if this pixel is involved in displaying the time (active)
-          bool active = digitMask[orientation%2][digit][i];
-          if(active)
-          {
-            int ledId;
-            //ledmap 1 => vertical pattern (as if strips were spreaded vertically)
-            if(ledmapEnabled && currentLedmap == 1)
-            {
-              ledId = reverseVerticalMap[digitSegment[orientation][p][i]];
-            }
-            //ledmap 0 => round pattern (as if strips were spreaded in circles)
-            else if(ledmapEnabled)
-            {
-              ledId = reverseRoundMap[digitSegment[orientation][p][i]];
-            }
-            //no ledmap
-            else
-            {
-              ledId = digitSegment[orientation][p][i];
-            }
-
-            //normal => white digits
-            if(!reverseDigits)
-            {
-              strip.setPixelColor(ledId, RGBW32(255*digitWhite,255*digitWhite,255*digitWhite,0));
-            }
-            // black digits instead of white
-            else 
-            {
-              //mark this digit as 'should be dark' to later set color
-              reverseMask[ledId]=true;
-            }
-          }
-        }
-      }
-      
-      //apply dark digits instead of white
-      if(reverseDigits)
-      {
-        for(int i=0; i<LEDS_NO; i++)
-        {
-          if(!reverseMask[i])
-          {
-            strip.setPixelColor(i, RGBW32(255*!digitWhite,255*!digitWhite,255*!digitWhite,0));
-          }
-        }
-      }
-
-    }
-
-   
     /*
      * getId() allows you to optionally give your V2 usermod an unique ID (please define it in const.h!).
      * This could be used in the future for the system to determine whether your usermod is installed.
      */
-    uint16_t getId()
-    {
-      return USERMOD_ID_HEXA_CLOCK;
-    }
+    uint16_t getId();
 
-   //More methods can be added in the future, this example will then be extended.
-   //Your usermod will remain compatible as it does not need to implement all methods from the Usermod base class!
+    //More methods can be added in the future, this example will then be extended.
+    //Your usermod will remain compatible as it does not need to implement all methods from the Usermod base class!
 };
